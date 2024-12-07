764c6bc31c31edbf25507a5d63360c8f4b5139ba
Ron Sprenkels <ron.sprenkels@gmail.com>
2024-12-07 22:24:36 +0100

    multiple logvalues in one request

--- src/data_logger.py
+++ src/data_logger.py
@@ -2,6 +2,7 @@ from datetime import date, datetime, time, timedelta
 import logging as log
 
 from pydantic import BaseModel
+from typing import List
 
 log.basicConfig(format='%(asctime)s - %(message)s', level=log.INFO)
 
@@ -16,9 +17,10 @@ class KeyValue(BaseModel):
     v: str
 
 @app.post("/log")
-def input_request(kv: KeyValue):
-    log.info(f"New request arrived: {kv}")
-    dao.add(kv.k, kv.v)
+def input_request(kv_list: List[KeyValue]):
+    log.info(f"New request arrived: {kv_list}")
+    for kv in kv_list:
+        dao.add(kv.k, kv.v)
 
 # start the application from a commandline prompt like this:
 # fastapi dev data_logger.py
