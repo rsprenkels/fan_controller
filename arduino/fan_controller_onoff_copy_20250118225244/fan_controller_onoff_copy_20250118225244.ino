15fb2ca980679ccdd34a170bd17b0aa5dd4acbc7
Ron Sprenkels <ron.sprenkels@gmail.com>
2025-01-18 22:55:04 +0100

    multiple logvalues in one request

--- arduino/fan_controller_onoff_copy_20250118225244/fan_controller_onoff_copy_20250118225244.ino
+++ arduino/fan_controller_onoff_copy_20250118225244/fan_controller_onoff_copy_20250118225244.ino
@@ -1,5 +1,4 @@
 /*
-  Ron Sprenkels
 */
 
 #include <ESP8266WiFi.h>
@@ -7,9 +6,8 @@
 #include <ArduinoJson.h>
 #include "DHT20.h"
 
-int ledState = LOW;
-const uint8_t pwm_fan_pin = D3;
-const uint8_t tacho_fan_pin = D2; 
+const int g_fan_pwm_pin = D3;
+const int g_fan_onoff_pin = D0;
 
 unsigned long previousMillis = 0;
 unsigned long loop_count = 0;
@@ -18,9 +16,8 @@ const int delay_millis = 20;
 const int dMin = 0;
 const int dMax = 255;
 
-unsigned long g_last_logged_at = 0;
-float g_temperature = -100.0;
-float g_humidity = -100.0;
+int g_previous_duty_cycle = -1;
+unsigned long g_last_setdc = 0;
 
 //                    +--------------+
 //  3V3       VDD ----| 1            |
@@ -29,20 +26,18 @@ float g_humidity = -100.0;
 //  D5        SCL ----| 4            |
 //                    +--------------+
 DHT20 DHT;
-
-int previous_duty_cycle = -1;
-
-int volatile tacho_pulse_count = 0;
-unsigned long tacho_measure_start_time;
-int fan_rpm = 0;
+unsigned long g_last_logged_at = 0;
+float g_temperature = -100.0;
+float g_humidity = -100.0;
 
 #define SERVER_IP "192.168.2.25"
 #ifndef STASSID
-#define STASSID "BE"
+#define STASSID "RSJB"
 #define STAPSK "Dbesbodk66"
 #endif
 
 void setup_wifi() {
+  Serial.printf(PSTR("connecting to WIFI: %s\n"), STASSID);
   WiFi.begin(STASSID, STAPSK);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -53,108 +48,79 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void setup() {
-  pinMode(pwm_fan_pin, OUTPUT);
-  analogWriteFreq(25000);
-  pinMode(tacho_fan_pin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(tacho_fan_pin), tachoCounter, RISING);
-
+void setup_dht20() {
   // for the DHT20 sensor on I2C
   Wire.begin(GPIO_ID_PIN(D4), GPIO_ID_PIN(D5));
   Wire.setClock(400000);
   DHT.begin();  //  ESP32 default 21, 22
-  Serial.println(DHT.requestData());
-
-  Serial.begin(115200);
-  Serial.println(F("\nFan controller and data logger"));
-
-  setup_wifi();
+  DHT.requestData();
 }
 
-
-void change_dutycycle() {
-  for(int dutyCycle = dMin; dutyCycle <= dMax; dutyCycle++){   
-    // changing the LED brightness with PWM
-    Serial.printf(PSTR("duty cycle %d\n"), dutyCycle);
-    analogWrite(pwm_fan_pin, dutyCycle);
-    delay(delay_millis);
-  }
-  delay(500);
-  // decrease the LED brightness
-  for(int dutyCycle = dMax; dutyCycle >= dMin; dutyCycle--){
-    // changing the LED brightness with PWM
-    Serial.printf(PSTR("duty cycle %d\n"), dutyCycle);
-    analogWrite(pwm_fan_pin, dutyCycle);
-    delay(delay_millis);
-  }
-  delay(500);
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.print("\n\n\n");
+  delay(1000);
+  pinMode(g_fan_pwm_pin, OUTPUT);
+  analogWriteFreq(25000);
+  pinMode(g_fan_onoff_pin, OUTPUT);
+  setup_dht20();
+  setup_wifi();
+  Serial.println(F("\nsetup complete"));
 }
 
 void set_dutycycle(int duty_cycle) {
-  analogWrite(pwm_fan_pin, 255 - duty_cycle);
-  Serial.printf(PSTR("duty cycle: %d\n"), duty_cycle);
-}
-
-int set_dc_based_on_voltage() {
-  int sensor_value = get_analog_reading();
-  int duty_cycle = min(255,max(0, int((sensor_value - 34) / 3.8)));
-  if (duty_cycle != previous_duty_cycle) {
-    previous_duty_cycle = duty_cycle;
-    set_dutycycle(duty_cycle);
-  }
-  return duty_cycle;
-}
-
-int set_dc_based_on_temperature() {
-  int duty_cycle;
-  if (g_temperature > 23.0) {
-    duty_cycle = set_dc_based_on_voltage();
+  if (duty_cycle >= 240) {
+    pinMode(g_fan_pwm_pin, INPUT);
+    digitalWrite(g_fan_onoff_pin, LOW);
+    Serial.printf(PSTR("duty cycle: %d (fan switched off)\n"), duty_cycle);
   } else {
-    duty_cycle = 255;
+    digitalWrite(g_fan_onoff_pin, HIGH);
+    pinMode(g_fan_pwm_pin, OUTPUT);
+    analogWriteFreq(25000);
+    analogWrite(g_fan_pwm_pin, 255 - duty_cycle);
+    Serial.printf(PSTR("duty cycle: %d (fan switched ON)\n"), duty_cycle);
   }
-  if (duty_cycle != previous_duty_cycle) {
-    previous_duty_cycle = duty_cycle;
-    set_dutycycle(duty_cycle);
-  }
-  return duty_cycle;
 }
 
-int get_analog_reading() {
-  int sensorValue = analogRead(A0);
-  return sensorValue;
-}
+void set_dc_based_on_voltage() {
+  uint32_t now = millis();
 
-ICACHE_RAM_ATTR void tachoCounter() {
-  tacho_pulse_count++;
-}
-
-void measure_rpm() {
-    for(int loop_cnt = 0; loop_cnt <= 1; loop_cnt++) {
-      tacho_measure_start_time = millis();
-      tacho_pulse_count = 0;
-      while ((millis() - tacho_measure_start_time) < 1000) {
-        // void
-      }
-      fan_rpm = tacho_pulse_count * 60 / 2;
-      Serial.printf(PSTR("Fan running at: %d rpm.\n"), fan_rpm);
+  if (now - g_last_setdc <= 1000) {
+    return;
+  } else {
+    g_last_setdc = now;
+  }  
+  int sensor_value = analogRead(A0);
+  int duty_cycle = min(255,max(0, int((sensor_value - 34) / 3.8)));
+  if (g_temperature >= 22.0) {
+    if (duty_cycle != g_previous_duty_cycle) {
+      g_previous_duty_cycle = duty_cycle;
+      set_dutycycle(duty_cycle);
+    }
+  } else {
+      duty_cycle = 255;  
+      g_previous_duty_cycle = duty_cycle;
+      set_dutycycle(duty_cycle);
   }
 }
 
 void read_dht20() {
   uint32_t now = millis();
 
-  if (now - DHT.lastRead() > 1000)
+  if (now - DHT.lastRead() > 2000)
   {
     DHT.readData();
     DHT.convert();
 
-    Serial.print(DHT.getHumidity(), 1);
-    Serial.print(" %RH \t");
     g_temperature = DHT.getTemperature(); 
     g_humidity = DHT.getHumidity(); 
+    Serial.print(g_humidity, 1);
+    Serial.print(" %RH \t");
     Serial.print(g_temperature, 1);
     Serial.print(" °C\t");
     Serial.println("");
+
     DHT.requestData();
   }
 }
@@ -177,16 +143,17 @@ void to_log_json(char *buf, float temp, float rel_hum) {
   serializeJson(payload, buf, 100); 
 }
 
-void log_temp_humidity(float temp, float humidity) {
-  
+void log_temp_humidity(float temp, float humidity) {  
   uint32_t now = millis();
 
-  if (now - g_last_logged_at <= 10000) {
+  if (now - g_last_logged_at <= 5000) {
     return;
   } else {
     g_last_logged_at = now;
   }
  
+  Serial.printf("WiFi status: %d\n", WiFi.status());
+
   if ((WiFi.status() == WL_CONNECTED)) {
 
     WiFiClient client;
@@ -208,6 +175,8 @@ void log_temp_humidity(float temp, float humidity) {
       Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
     http.end();
+  } else {
+      Serial.printf("not connected to Wifi, so not logging\n");
   }
 }
 
@@ -215,8 +184,6 @@ void log_temp_humidity(float temp, float humidity) {
 void loop() {
   loop_count += 1;
   set_dc_based_on_voltage();
-  // set_dc_based_on_temperature();
-  measure_rpm();
   read_dht20();
   log_temp_humidity(g_temperature, g_humidity);
 }
